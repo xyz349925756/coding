c767d29355fbb3d12fdeff457a1f5115f9b3cad6
xyz349925756 <xyz349925756@hotmail.com>
2022-01-10 22:28:28 +0800

    Changes

--- study_python_2022/day2/for_python.py
+++ study_python_2022/day2/for_python.py
@@ -1,4 +1,12 @@
 # /usr/bin/python
 # coding --utf-8--
-for i in range(10):
-    print(i)
+# for i in range(10):   #ctrl+?
+#     print(i)
+for a in range(5,10):
+    print(a)
+
+#可以自定义开始
+
+#也可以跳过取数据
+for a in range(5,10,2):
+    print(a)
