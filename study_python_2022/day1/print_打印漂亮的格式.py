be4a940157ec6b24113ff8d5ae8327ef5b1acd8a
xyz349925756 <xyz349925756@hotmail.com>
2022-01-10 22:07:02 +0800

    Changes

---
+++ study_python_2022/day1/print_打印漂亮的格式.py
@@ -0,0 +1,17 @@
+# /usr/bin/python
+# coding --utf-8--
+
+name = input("please input your name:")
+age = input("please input your age:")
+job = input("please input your job:")
+address = input("please input your address:")
+
+mags=f''' 
+-------------{name} Info----------
+name    : {name}
+age     : {age}
+job     : {job}
+address : {address}
+-----------------------------------
+'''
+print(mags)
