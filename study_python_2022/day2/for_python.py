dbfc558d52665e233a038908df02043aa834f55b
xyz349925756 <xyz349925756@hotmail.com>
2022-01-10 22:53:06 +0800

    Changes

--- study_python_2022/day2/for_python.py
+++ study_python_2022/day2/for_python.py
@@ -2,11 +2,24 @@
 # coding --utf-8--
 # for i in range(10):   #ctrl+?
 #     print(i)
-for a in range(5,10):
-    print(a)
+# for a in range(5,10):
+#     print(a)
 
 #可以自定义开始
 
 #也可以跳过取数据
-for a in range(5,10,2):
-    print(a)
+# for a in range(5,10,2):
+#     print(a)
+
+# for i in range(0,100,1):
+#     print(i)
+
+# 这里1是奇数 2是偶数
+
+for i in range(100):
+    if i % 2 == 0:
+        print(i,"偶数")
+    else:
+        print(i,"奇数")
+
+exit("good bye!")  #退出循环
---
+++ study_python_2022/day2/嵌套循环.py
@@ -0,0 +1,8 @@
+# /usr/bin/python
+# coding --utf-8--
+
+for i in range(1,6):
+    print(f"L",i,"层")
+    for j in range(1,9):
+        print(f"L{i}-{i}0{j} 室")
+
