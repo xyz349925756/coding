55a5d4da3707344d39a76d57cfafa2c408b723d2
xyz349925756 <xyz349925756@hotmail.com>
2022-04-30 16:04:07 +0800

    update

--- study_python_2022/day5/1.py
+++ study_python_2022/day5/1.py
@@ -4,5 +4,23 @@
 # FILE: 1.py
 # TIME: 2022/3/13 周日
 
-for i in range(10):
-    print(i)
+# 循环打印1-10
+#for i in range(10):
+#    msg = f'''
+#    输出：{i}
+#    '''
+#    print(msg)
+
+name = input("请输入姓名：")
+qq = input("请输入QQ号码：")
+tel = input("请输入手机号：")
+address = input("请输入地址：")
+msg = f'''
+=======================
+姓名：{name}
+QQ:{qq}
+手机号：{tel}
+地址：{address}
+=======================
+'''
+print(msg)
