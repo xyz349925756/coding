c8b65e991ff54e458c8b6ce9164398b9978db577
xyz349925756 <xyz349925756@hotmail.com>
2022-06-10 22:48:48 +0800

    Changes

---
+++ project/ATM/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+# Auth: xyz34
+# File: setup.py
+# TIME: 2022/6/10 星期五  周五
+
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+print(os.path.dirname(__file__))
